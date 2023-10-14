dbba8988c750a4147a56f9ddfbb41fbb70e04458
htqnhat280203 <htqnhat280203@gmail.com>
2023-10-14 13:13:44 +0700

    first commit

---
+++ src/main/Main.java
@@ -0,0 +1,14 @@
+package main;
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+
+import controller.DictionaryManagement;
+
+public class Main {
+    public static void main(String[] args) {
+        new DictionaryManagement().run();
+    }
+}
